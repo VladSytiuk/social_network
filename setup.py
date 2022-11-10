24012f860cb1d5ea6a0acfe0597c9573f8a8ce02
VladSytiuk <79190084+VladSytiuk@users.noreply.github.com>
2022-11-10 20:21:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
+from datetime import date
 
 setup(
     name='samplepackage',
-    version=1,
+    version=date.today().strftime('%y.%m.%d'),
     packages=['posts'],
     include_package_data=True,
     zip_safe=False
