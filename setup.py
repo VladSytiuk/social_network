ba383babe3e9574be44205b57c970da83b2de037
VladSytiuk <79190084+VladSytiuk@users.noreply.github.com>
2022-11-10 16:00:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='samplepackage',
-    version='0.2',
+#    version='0.2',
     packages=['posts'],
     include_package_data=True,
     zip_safe=False
